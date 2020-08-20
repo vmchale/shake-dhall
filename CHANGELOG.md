70d22b24814b22768ae8c50eb2035a3868227915
Vanessa McHale <vanessa.mchale@bitnomial.com>
2020-08-20 13:10:58 -0500

    Add dhall cli-based bits

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # shake-dhall
 
+## 0.1.1.0
+
+  * Add functions `dhallDeps` and `needDhallCli` in `Development.Shake.Dhall`
+
 ## 0.1.0.0
 
 Initial release
--- src/Development/Shake/Dhall.hs
+++ src/Development/Shake/Dhall.hs
@@ -1,12 +1,32 @@
 module Development.Shake.Dhall ( needDhall
+                               , needDhallCli
+                               , dhallDeps
                                ) where
 
+import           Control.Monad             (filterM, (<=<))
 import           Control.Monad.IO.Class    (liftIO)
 import           Data.Containers.ListUtils (nubOrd)
-import           Development.Shake         (Action, need)
+import           Development.Shake         (Action, Stdout (Stdout), command,
+                                            doesFileExist, need)
 import           Dhall.Dep
 
 -- | 'need' some @.dhall@ files and imported dependencies
 needDhall :: [FilePath] -> Action ()
 needDhall fps =
     need =<< liftIO (nubOrd . concat . (fps:) <$> traverse getAllFileDeps fps)
+
+-- | Same as 'needDhallCli' but shells out to the command-linen executable
+--
+-- @since 0.1.1.0
+needDhallCli :: [FilePath] -> Action ()
+needDhallCli =
+    need . concat <=< traverse dhallDeps
+
+-- | Uses @dhall resolve --transitive-dependencies@ to work; command-line tool
+-- must be installed.
+--
+-- @since 0.1.1.0
+dhallDeps :: FilePath -> Action [FilePath]
+dhallDeps inp = do
+    (Stdout out) <- command [] "dhall" ["resolve", "--transitive-dependencies", "--file", inp]
+    (inp:) <$> filterM doesFileExist (lines out)
